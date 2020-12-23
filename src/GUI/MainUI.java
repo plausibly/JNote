3907dbc4c3185c5431e9b0ecb5c1f5873e9b0a5d
pouya.gg <pouyaghi123@gmail.com>
2020-12-23 16:31:33 -0500

    Slight changes to front-end

---
+++ src/GUI/MainUI.java
@@ -0,0 +1,185 @@
+package GUI;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+
+
+public class MainUI extends Application {
+    private TextArea txtInput;
+    private Stage pStage;
+    private boolean newChanges = false;
+    private Font txtFont;
+
+    /**
+     * Request to save/load to a file.
+     * @param txt   The TextArea object containing the input being saved
+     * @param stage The Stage object of the UI
+     * @param type  Boolean, true means save the file and false is load a file
+     */
+    public void IORequest(TextArea txt, Stage stage, boolean type) {
+        FileChooser choose = new FileChooser();
+        choose.getExtensionFilters().addAll(new ExtensionFilter("Text File", "*.txt"),
+                new ExtensionFilter("All Files", "*.*"));
+
+        if (type) {
+            System.out.println("Save requested...");
+            choose.setTitle("Save File");
+            File fSave = choose.showSaveDialog(stage);
+            if (fSave != null) {    // need to fix so it maintains formatting
+                System.out.println("Writing...");
+                try {
+                    BufferedWriter writeFile = new BufferedWriter(new FileWriter(fSave));
+                    writeFile.write(txt.getText()); // need to maintain FORMATTING!!
+                    writeFile.close();
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            } else {
+                System.out.println("cancelled");
+            }
+
+        } else {
+            System.out.println("Open iniat");
+            choose.setTitle("Open File");
+            File fOpen = choose.showOpenDialog(stage);
+
+            if (fOpen != null) {
+                try {
+                    txt.setText("");
+                    BufferedReader rFile = new BufferedReader(new FileReader(fOpen));
+                    String curr = "";
+
+                    rFile.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            } else {
+                System.out.println("Cancelled");
+            }
+
+        }
+
+    }
+
+ 
+    public void TextFormat(TextArea txt, Font fnt, Stage stage, String type) {}
+    
+    @Override
+    /**
+     * Starting the GUI
+     * @param primaryStage The Stage object of the UI
+     */
+    public void start(Stage primaryStage) {
+        this.pStage = new Stage();
+        this.pStage.setTitle("JNote - Java-powered Notebook");
+        BorderPane bPane = new BorderPane();
+
+        this.txtFont = Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 15);
+
+        this.txtInput = new TextArea();
+        this.txtInput.setFont(this.txtFont);
+        this.txtInput.setWrapText(true);
+
+        // ** Menu Objects **
+        MenuBar mBar = new MenuBar();
+
+        Menu mFile = new Menu("File");
+        Menu mEdit = new Menu("Edit");
+
+        MenuItem mNew = new MenuItem("New");
+        MenuItem mSave = new MenuItem("Save");
+        MenuItem mOpen = new MenuItem("Open");
+        MenuItem mBold = new MenuItem("Bold");
+        MenuItem mItalic = new MenuItem("Italics");
+        MenuItem mFont = new MenuItem("Fonts");
+        MenuItem mFind = new MenuItem("Find");
+
+        mNew.setOnAction(e -> {
+            System.out.println("New");
+        });
+        mSave.setOnAction(e -> {
+            IORequest(this.txtInput, this.pStage, true);
+        });
+        mOpen.setOnAction(e -> {
+            IORequest(this.txtInput, this.pStage, false);
+        });
+        this.pStage.setOnCloseRequest(e -> {
+            if (this.newChanges) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to save changes before closing?",
+                        ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+                
+                alert.setTitle("Unsaved Changes");
+                ButtonType res = alert.showAndWait().orElse(ButtonType.CANCEL);
+                if (res.equals(ButtonType.YES)) {
+                    System.out.println("Save requeted");
+                    IORequest(this.txtInput, this.pStage, true);
+                } else if (res.equals(ButtonType.NO))
+                    System.out.println("No save. Close ");
+                else
+                    e.consume();
+
+            }
+        });
+
+        mBold.setOnAction(e -> {
+            TextFormat(this.txtInput, this.txtFont, this.pStage, ((MenuItem) e.getSource()).getText());
+        });
+        mItalic.setOnAction(e -> {
+            TextFormat(this.txtInput, this.txtFont, this.pStage, ((MenuItem) e.getSource()).getText());
+        });
+        mFont.setOnAction(e -> {
+            TextFormat(this.txtInput, this.txtFont, this.pStage, ((MenuItem) e.getSource()).getText());
+        });
+
+        mNew.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
+        mSave.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
+        mOpen.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
+        mBold.setAccelerator(new KeyCodeCombination(KeyCode.B, KeyCombination.CONTROL_DOWN));
+        mItalic.setAccelerator(new KeyCodeCombination(KeyCode.I, KeyCombination.CONTROL_DOWN));
+        mFind.setAccelerator(new KeyCodeCombination(KeyCode.F, KeyCombination.CONTROL_DOWN));
+
+        // ** Add objects **
+        mFile.getItems().addAll(mNew, mSave, mOpen);
+        mEdit.getItems().addAll(mFont, mBold, mItalic);
+        mBar.getMenus().addAll(mFile, mEdit);
+
+        bPane.setTop(mBar);
+        bPane.setCenter(this.txtInput);
+
+        // ** Some Initializations **
+        Scene myScene = new Scene(bPane, 1030, 600);
+        this.pStage.setScene(myScene);
+        this.pStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
